8e005ec92118e28c81b5134b4707b554bb18ee12
Sruthy Nair <sruthysnair9847@gmail.com>
2026-08-03 15:24:02 +0530

    Improve optimization objective, metrics, and report output

--- src/optimization/objective.py
+++ src/optimization/objective.py
@@ -2,14 +2,17 @@ from pulp import lpSum
 
 def create_objective(model, optimization_data, decision_vars):
     """
-    Objective:
-    Minimize shipping days and late delivery risk.
+    Multi-objective optimization:
+    - Minimize shipping days
+    - Minimize late delivery risk
+    - Maximize profit
     """
 
     model += lpSum(
         (
-            optimization_data.loc[i, "days_for_shipping_real"]
+            2 * optimization_data.loc[i, "days_for_shipping_real"]
             + 10 * optimization_data.loc[i, "late_delivery_risk"]
+            - 0.05 * optimization_data.loc[i, "benefit_per_order"]
         ) * decision_vars[i]
         for i in optimization_data.index
     )
